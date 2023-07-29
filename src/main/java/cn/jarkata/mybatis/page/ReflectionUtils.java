c2e9d6d9d0e5b95123aac11b9dd846f6bd5f7976
Jarkata <jarkata@foxmail.com>
2023-07-29 14:29:34 +0800

    fix:分页查询参数支持对象

--- src/main/java/cn/jarkata/mybatis/page/ReflectionUtils.java
+++ src/main/java/cn/jarkata/mybatis/page/ReflectionUtils.java
@@ -1,9 +1,13 @@
 package cn.jarkata.mybatis.page;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ReflectionUtils {
+
+    private static final Map<Class<?>, Field> fieldCache = new ConcurrentHashMap<>();
 
     public static Object getFieldValue(Object obj, String fieldName) {
         if (Objects.isNull(obj)) {
@@ -30,11 +34,17 @@ public class ReflectionUtils {
         if (Object.class.getSimpleName().equals(clazz.getSimpleName())) {
             return null;
         }
-        Field declaredField;
+        Field declaredField = fieldCache.get(clazz);
+        if (Objects.nonNull(declaredField)) {
+            return declaredField;
+        }
         try {
             declaredField = clazz.getDeclaredField(fieldName);
         } catch (Exception ex) {
-            return getField(clazz.getSuperclass(), fieldName);
+            declaredField = getField(clazz.getSuperclass(), fieldName);
+        }
+        if (Objects.nonNull(declaredField)) {
+            fieldCache.put(clazz, declaredField);
         }
         return declaredField;
     }
